c5dd9cd5d30dfb275b9f6189d2b378c48b455fb7
calesj <cales.junior2002@gmail.com>
2026-03-14 22:06:58 -0300

    install ziggy.js remove wayfinder add Auth controllers

---
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SendPasswordResetLinkRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Password;
+use Inertia\Inertia;
+use Inertia\Response;
+
+final class PasswordResetLinkController extends Controller
+{
+    public function create(): Response
+    {
+        return Inertia::render('user-password/forgot');
+    }
+
+    public function store(SendPasswordResetLinkRequest $request): RedirectResponse
+    {
+        $status = Password::sendResetLink($request->only('email'));
+
+        if ($status === Password::RESET_LINK_SENT) {
+            return back()->with('success', __($status));
+        }
+
+        return back()->withErrors(['email' => __($status)]);
+    }
+}
---
+++ app/Http/Requests/CreateUserPasswordRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+final class CreateUserPasswordRequest extends FormRequest
+{
+    /**
+     * @return array<string, array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'token' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed', Password::defaults()],
+        ];
+    }
+}
